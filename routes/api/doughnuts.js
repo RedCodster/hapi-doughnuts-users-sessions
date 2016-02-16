cf2a5a54377cba043a3146966828b7d0a1127035
RedCodster <rob_fong00@yahoo.com>
2016-02-16 16:51:24 +0800

    working version

--- routes/api/doughnuts.js
+++ routes/api/doughnuts.js
@@ -15,7 +15,26 @@ exports.register = function (server, options, next) {
         });
       }
     },
-    { // Creating a new doughnut record.
+    {
+      method: 'GET',
+      path: '/api/{username}/doughnuts',
+      handler: function (request, reply) {
+        var db = request.server.plugins['hapi-mongodb'].db;
+        var username = request.params.username;
+
+        //search the username and exract the id of the user
+        db.collection('users').findOne({username: username}, function (err,user) {
+          if (err) { return reply(err).code(400); }
+        // given the user_id, we will find all the doughnuts created by this user
+          var user_id = user._id;
+          db.collection('doughnuts').find({user_id: user_id}).toArray(function (err, results) {
+            if (err) { return reply(err).code(400);}
+            reply(results).code(200);
+          });
+        });
+      }
+    },
+    { // Creating a new doughnut
       method: 'POST',
       path: '/api/doughnuts',
       handler: function (request, reply) {
@@ -27,7 +46,7 @@ exports.register = function (server, options, next) {
             var session = request.yar.get('hapi_doughnuts_session');
 
             var doughnut = {
-              userID: ObjectID(session.user_id),
+              user_id: ObjectID(session.user_id),
               style: request.payload.style,
               flavor: request.payload.flavor
             };
@@ -42,7 +61,7 @@ exports.register = function (server, options, next) {
         });
       }
     },
-    { // Delete doughnut only if it is yours to begin with.
+    { // Delete a doughnut only if it is yours to begin with.
       method: 'DELETE',
       path: '/api/doughnuts/{id}',
       handler: function (request, reply) {
@@ -86,7 +105,7 @@ exports.register = function (server, options, next) {
             var ObjectID = request.server.plugins['hapi-mongodb'].ObjectID;
             var session = request.yar.get('hapi_doughnuts_session');
             var id = ObjectID(request.params.id);
-            var user_id = ObjectID(session.user.id);
+            var user_id = ObjectID(session.user_id);
             var updateDoughnut = {
               style: request.payload.style,
               flavor: request.payload.flavor
@@ -100,9 +119,9 @@ exports.register = function (server, options, next) {
               }
             //if doughnut's user_id is the same as session user, delete doughnut
               if (doughnut.user_id.toString() === user_id.toString()) {
-                db.collection('doughnuts').update({"_id": id}, updateDoughnut, function (err, doughnut) {
+                db.collection('doughnuts').update({"_id": id}, {$set: updateDoughnut}, function (err, doughnut) {
                   if (err) { return reply(err).code(400); }
-                  reply(doc).code(200);
+                  reply(doughnut).code(200);
                 });
               } else {
                 reply({message: 'This is not your doughnut!'}).code(400);
